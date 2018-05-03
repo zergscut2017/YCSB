40442e8085c2a73e56203e94410557ee1ae7c532
root <root@localhost.localdomain>
2018-05-03 01:04:16 -0700

    create NFProfile, workload, client and new load type geodeprofile

---
+++ core/src/main/java/com/yahoo/ycsb/NFProfile.java
@@ -0,0 +1,59 @@
+package com.yahoo.ycsb;
+
+public class NFProfile {
+
+    private String nfProfileID;
+
+    private String nfType;
+
+    private String plmn;
+
+    private NFSingleNssai snssai;
+
+
+    public NFProfile() {
+    }
+
+    public NFProfile(String nfProfileID, String nfType, String plmn, NFSingleNssai snssai) {
+        this.nfProfileID = nfProfileID;
+        this.nfType = nfType;
+        this.plmn = plmn;
+        this.snssai = snssai;
+    }
+
+    public String getNfProfileID() {
+        return nfProfileID;
+    }
+
+    public void setNfProfileID(String nfProfileID) {
+        this.nfProfileID = nfProfileID;
+    }
+
+    public String getNfType() {
+        return nfType;
+    }
+
+    public void setNfType(String nfType) {
+        this.nfType = nfType;
+    }
+
+    public String getPlmn() {
+        return plmn;
+    }
+
+    public void setPlmn(String plmn) {
+        this.plmn = plmn;
+    }
+
+    public NFSingleNssai getSnssai() {
+        return snssai;
+    }
+
+    public void setSnssai(NFSingleNssai snssai) {
+        this.snssai = snssai;
+    }
+
+	
+	
+	
+}
---
+++ core/src/main/java/com/yahoo/ycsb/NFSingleNssai.java
@@ -0,0 +1,31 @@
+package com.yahoo.ycsb;
+
+public class NFSingleNssai {
+	
+    private int sd;
+    private int sst;
+
+    public NFSingleNssai() {
+    }
+
+    public NFSingleNssai(int sd, int sst) {
+        this.sd = sd;
+        this.sst = sst;
+    }
+
+    public int getSd() {
+        return sd;
+    }
+
+    public void setSd(int sd) {
+        this.sd = sd;
+    }
+
+    public int getSst() {
+        return sst;
+    }
+
+    public void setSst(int sst) {
+        this.sst = sst;
+    }
+}
